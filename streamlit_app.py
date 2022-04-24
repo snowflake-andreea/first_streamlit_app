009c8e324081b9ce88d330f65b14f89163c7a741
Andreea Adascalite <104313588+snowflake-andreea@users.noreply.github.com>
2022-04-24 18:21:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.text('Orange 3 & Blueberry')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Build Free-Range Egg')
