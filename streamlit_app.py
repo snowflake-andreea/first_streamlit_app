c6ae01395ca3f4e4f7f4789b342d8a7f1c469912
Andreea Adascalite <104313588+snowflake-andreea@users.noreply.github.com>
2022-04-27 09:03:44 +0300

    Update streamlit_app.py
    resolved typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruitvice Fruit Advice')
 import requests
 
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
